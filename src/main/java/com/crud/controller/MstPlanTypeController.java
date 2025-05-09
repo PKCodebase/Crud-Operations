a64ac8be7f806ca9e6faf4b35b76f7f4f0cf3f4c
PKCodebase <kaushikprasad1659@gmail.com>
2025-05-09 10:56:30 +0530

    Swagger Configuration

--- src/main/java/com/crud/controller/MstPlanTypeController.java
+++ src/main/java/com/crud/controller/MstPlanTypeController.java
@@ -2,6 +2,7 @@ package com.crud.controller;
 
 import com.crud.entity.MstPlanType;
 import com.crud.service.impl.MstPlanTypeServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,26 +19,31 @@ public class MstPlanTypeController {
     }
 
     @PostMapping("/add")
+    @Operation(summary = "ADD")
     public ResponseEntity<MstPlanType> add(@RequestBody MstPlanType mstPlanType){
         return ResponseEntity.ok(mstPlanTypeServiceImpl.add(mstPlanType));
     }
 
     @PutMapping("/update/{guid}")
+    @Operation(summary = "UPDATE")
     public ResponseEntity<MstPlanType> update(@PathVariable String guid, @RequestBody MstPlanType updatedData){
         return ResponseEntity.ok(mstPlanTypeServiceImpl.updateByGuid(guid, updatedData));
     }
 
     @DeleteMapping("/delete/{guid}")
+    @Operation(summary="DELETE")
     public void delete(@PathVariable String guid){
          mstPlanTypeServiceImpl.deleteByGuid(guid);
     }
 
     @GetMapping("/all")
+    @Operation(summary="GET ALL")
     public List<MstPlanType> getAll(){
         return mstPlanTypeServiceImpl.findAll();
     }
 
     @GetMapping("/getById/{guid}")
+    @Operation(summary="GET BY ID")
     public ResponseEntity<MstPlanType> getByGuid(@PathVariable String guid){
         return ResponseEntity.ok(mstPlanTypeServiceImpl.getByGuid(guid));
     }
