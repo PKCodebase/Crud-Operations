8c643d526c536939d5f748b1b05810f63e9e6d60
PKCodebase <kaushikprasad1659@gmail.com>
2025-05-13 12:04:00 +0530

    Add security

---
+++ src/main/java/com/crud/configuration/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.crud.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(HttpMethod.GET, "/plan/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
--- src/main/java/com/crud/controller/MstPlanTypeController.java
+++ src/main/java/com/crud/controller/MstPlanTypeController.java
@@ -3,13 +3,16 @@ package com.crud.controller;
 import com.crud.entity.MstPlanType;
 import com.crud.service.impl.MstPlanTypeServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/mst-plan")
+@RequestMapping("/plan")
+@Tag(name="Mst Plan Controller")
 public class MstPlanTypeController {
 
     private final MstPlanTypeServiceImpl mstPlanTypeServiceImpl;
@@ -18,32 +21,43 @@ public class MstPlanTypeController {
         this.mstPlanTypeServiceImpl = mstPlanTypeServiceImpl;
     }
 
+    @Operation(summary = "ADD" , description = "Add new plan")
+    @ApiResponse(
+            responseCode = "200", description = "Successfully added"
+    )
     @PostMapping("/add")
-    @Operation(summary = "ADD")
     public ResponseEntity<MstPlanType> add(@RequestBody MstPlanType mstPlanType){
         return ResponseEntity.ok(mstPlanTypeServiceImpl.add(mstPlanType));
     }
 
+
+    @Operation(summary = "UPDATE BY ID" , description = "Update plan by id")
+    @ApiResponse(
+            responseCode = "200", description = "Successfully updated"
+    )
     @PutMapping("/update/{guid}")
-    @Operation(summary = "UPDATE")
     public ResponseEntity<MstPlanType> update(@PathVariable String guid, @RequestBody MstPlanType updatedData){
         return ResponseEntity.ok(mstPlanTypeServiceImpl.updateByGuid(guid, updatedData));
     }
 
+
+    @Operation(summary="DELETE BY ID" , description="Delete plan")
+    @ApiResponse(responseCode = "200", description = "Deleted Successfully")
     @DeleteMapping("/delete/{guid}")
-    @Operation(summary="DELETE")
     public void delete(@PathVariable String guid){
          mstPlanTypeServiceImpl.deleteByGuid(guid);
     }
 
+    @Operation(summary="GET ALL",description = "Get all plan")
+    @ApiResponse(responseCode = "200", description = "Successfully fetched all")
     @GetMapping("/all")
-    @Operation(summary="GET ALL")
     public List<MstPlanType> getAll(){
         return mstPlanTypeServiceImpl.findAll();
     }
 
+    @Operation(summary="GET BY ID" , description = "Get plan by id")
+    @ApiResponse(responseCode = "200", description = "Successfully fetched By Id")
     @GetMapping("/getById/{guid}")
-    @Operation(summary="GET BY ID")
     public ResponseEntity<MstPlanType> getByGuid(@PathVariable String guid){
         return ResponseEntity.ok(mstPlanTypeServiceImpl.getByGuid(guid));
     }
